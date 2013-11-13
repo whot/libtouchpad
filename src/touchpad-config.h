bf869ca92bffa72ba0ec16428d47eda433c12e21
Peter Hutterer <peter.hutterer@who-t.net>
2013-11-13 16:03:26 +1000

    Improve documentation to something mostly useful
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/touchpad-config.h
+++ src/touchpad-config.h
@@ -27,17 +27,94 @@
 
 #include <touchpad.h>
 
+/**
+ * @defgroup configuration Parameter configuration
+ *
+ * This section describes the parameter changes to adjust behavior and
+ * functionality.
+ */
+
+/**
+ * @ingroup configuration
+ *
+ * Modify tapping-related parameters. See @ref tapping for a detailed
+ * description.
+ *
+ * @param tp A previously opened touchpad device
+ * @param enable True to enable tapping, false to disable tapping
+ * @param timeout Maximum time in ms to elapse after a touch-and-hold for a
+ * a release to trigger a tap event.
+ * @param doubletap_timeout Maximum time in ms to elapse between two taps to
+ * count as doubletap.
+ * @param move_threshold Maximum movement in device coordinates before a
+ * release won't trigger a tap.
+ *
+ * @return 0 on success, -1 on failure
+ */
 int touchpad_config_tap_set(struct touchpad *tp, bool enable,
 			    int timeout, int doubletap_timeout,
 			    int move_threshold);
+/**
+ * @ingroup configuration
+ *
+ * Get tapping-related parameters. See @ref tapping for a detailed
+ * description.
+ *
+ * @param tp A previously opened touchpad device
+ * @param enabled If not NULL, set to true if tapping is enabled or false if
+ * tapping is disabled.
+ * @param timeout If not NULL, set to the tapping timeout
+ * @param doubletap_timeout If not NULL, set to the doubletap timeout
+ * @param move_threshold If not NULL, set to the move threshold
+ *
+ * @return 0 on success, -1 on failure
+ */
 int touchpad_config_tap_get(struct touchpad *tp, bool *enabled,
 			    int *timeout, int *doubletap_timeout,
 			    int *move_threshold);
+/**
+ * @ingroup configuration
+ * Set the touchpad's tapping configuration to the built-in defaults.
+ * @param tp A previously opened touchpad device
+ *
+ * @return 0 on success, -1 on failure
+ */
 int touchpad_config_tap_set_defaults(struct touchpad *tp);
 
+/**
+ * @ingroup configuration
+ *
+ * Modify scrolling-related parameters. See @ref scrolling for a detailed
+ * description.
+ *
+ * @param tp A previously opened touchpad device
+ * @param methods A bitmask of scrolling methods to enable
+ * @param vdelta Delta movement in device coordinates that represents one
+ * unit scrolling vertically
+ * @param hdelta Delta movement in device coordinates that represents one
+ * unit of scrolling horizontally
+ *
+ * @return 0 on success, -1 on failure
+ */
 int touchpad_config_scroll_set(struct touchpad *tp,
 			       enum touchpad_scroll_methods methods,
 			       int vdelta, int hdelta);
+/**
+ * @ingroup configuration
+ *
+ * Get scrolling-related parameters. See @ref scrolling for a detailed
+ * description.
+ *
+ * @param tp A previously opened touchpad device
+ * @param methods If not NULL, set to the bitmask of scrolling methods
+ * currently enabled
+ * @param vdelta If not NULL, set to the delta movement in device
+ * coordinates that represents one unit scrolling vertically
+ * @param hdelta if not NULL, set to the delta movement in device
+ * coordinates that represents one unit of scrolling horizontally
+ 
+ * @return 0 on success, -1 on failure
+ */
 int touchpad_config_scroll_get(struct touchpad *tp,
 			       enum touchpad_scroll_methods *methods,
 			       int *vdelta, int *hdelta);
